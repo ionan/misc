112fdfe7150df8cd1603e3d1fcfe6c2a82970130
ionan <ionan.gurru@gmail.com>
2018-07-04 17:46:54 +0200

    Minor changes

--- Podcasts/update-feeds.sh
+++ Podcasts/update-feeds.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-phantomjs job.js
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+echo "$DATE Start" >> execution_log.txt
+/usr/local/bin/phantomjs job.js
 git add *xml
 git commit -m "Update feeds"
 git push
+ENDDATE=`date '+%Y-%m-%d %H:%M:%S'`
+echo "$ENDDATE End" >> execution_log.txt
