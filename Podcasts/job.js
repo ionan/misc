86e0af167a4134c970b26b4a8d2860d516ef23fb
Ionan <ionan.gurru@gmail.com>
2020-05-31 07:00:54 +0200

    Node fixes

--- Podcasts/job.js
+++ Podcasts/job.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 var webPage = require('webpage');
 var page = webPage.create();
 page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
